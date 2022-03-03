b3124de21c811fbb9c665507404edfb7c565b647
TetraCarbonyl <100367024+TetraCarbonyl@users.noreply.github.com>
2022-03-03 09:37:41 +0300

    Add files via upload

---
+++ ConsoleUtilities/ClassLibrary/MyFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class MyFunctions
+    {
+        public string StringFrom1toN(int N)
+        {
+            string OutputString = null;
+
+            for (int i = 1; i <= N; i++)
+            {
+                OutputString += i.ToString();
+
+                if (i != N)
+                {
+                    OutputString += ',';
+                }
+            }
+            return OutputString;
+        }
+
+        public void RectangleAsterix(int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if ((i != j) || (i != (N - 1) / 2))
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.Write('\n');
+            }
+        }
+    }
+}
